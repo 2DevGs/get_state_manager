1ac44c6677b6889e73486a2c62fd86b77bb95858
2DevGs <dev.gustavoserrano@gmail.com>
2025-07-11 16:45:13 -0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get_state_manager/home_page.dart';
+import 'package:get_state_manager/pages/basico/reatividade_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      getPages: [
+        GetPage(name: '/', page: () => const HomePage()),
+        GetPage(name: '/basico', page: () => ReatividadePage()),
+      ],
+    );
+  }
+}
